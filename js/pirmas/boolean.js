529ca11d20f33c63bc3ccce44a3d32a5b2494b61
vitaliju <167984771+vitaliju@users.noreply.github.com>
2024-05-04 14:36:24 +0300

    boolean test

---
+++ js/pirmas/boolean.js
@@ -0,0 +1,154 @@
+
+
+console.log(true && true && true && true);
+console.log(true && true && true && false);
+console.log(true && true && false && true);
+console.log(true && false && true && true);
+console.log(false && true && true && true);
+console.log(true && true && false && false);
+console.log(true && false && true && false);
+console.log(true && false && false && true);
+console.log(false && false && true && true);
+console.log(false && truee && false && true);
+console.log(false && true && true && false);
+console.log(false && false && false && true);
+console.log(false && false && true && false);
+console.log(false && true && false && false);
+console.log(true && false && false && false);
+console.log(false && false && false && false);
+
+console.log('...............................');
+
+console.log(true || true || true || true);
+console.log(true || true || true || false);
+console.log(true || true || false || true);
+console.log(true || false || true || true);
+console.log(false || true || true || true);
+console.log(false || false || false || true);
+console.log(false || false || true || false);
+console.log(false || true || false || false);
+console.log(true || false || false || false);
+console.log(true || true || false || false);
+console.log(true || false || true || false);
+console.log(true || false || false || true);
+console.log(false || false || true || true);
+console.log(false || true || false || true);
+console.log(false || true || true || false);
+console.log(false || false || false || false);
+
+console.log('...............................');
+
+console.log(true && true && true || true);
+console.log(true && true && true || false);
+console.log(true && true && false || true);
+console.log(true && false && true || true);
+console.log(false && true && true || true);
+console.log(true && true && false || false);
+console.log(true && false && true || false);
+console.log(true && false && false || true);
+console.log(false && true && true || false);
+console.log(false && true && false || true);
+console.log(false && false && true || true);
+console.log(false && false && false || false);
+console.log(false && false && false|| true);
+console.log(false && false && true || false);
+console.log(false && true && false || false);
+console.log(true && false && false || false);
+
+console.log('.............................');
+
+console.log(true && true || true && true);
+console.log(true && true || true && false);
+console.log(true && true || false && true);
+console.log(true && false || true && true);
+console.log(false && true || true && true);
+console.log(true && true || false && false);
+console.log(true && false || true && false);
+console.log(true && false || false && true);
+console.log(false && false || true && true);
+console.log(false && true || false && true);
+console.log(false && true || true && false);
+console.log(false && true || false && false);
+console.log(false && false || true && false);
+console.log(false && false || false && true);
+console.log(false && false || false && false);
+console.log(true && false || false && false);
+
+console.log('...............................');
+
+console.log(true || true && true && true);
+console.log(true || true && true && false);
+console.log(true || true && false && true);
+console.log(true || false && true && true);
+console.log(false || true && true && true);
+console.log(true || true && false && false);
+console.log(true || false && true && false);
+console.log(true || false && false && true);
+console.log(false || false && true && true);
+console.log(false || true && false && true);
+console.log(false || true && true && false);
+console.log(false || false && false && true);
+console.log(false || false && true && false);
+console.log(false || true && false && false);
+console.log(false || false && false && false);
+console.log(true || false && false && false);
+
+console.log('...............................');
+
+console.log(true || true || true && true);
+console.log(true || true || true && false);
+console.log(true || true || false && true);
+console.log(true || false || true && true);
+console.log(false || true || true && true);
+console.log(true || true || false && false);
+console.log(true || false || true && false);
+console.log(true || false || false && true);
+console.log(false || false || true && true);
+console.log(false || true || false && true);
+console.log(false || true || true && false);
+console.log(false || false || false && true);
+console.log(false || false || true && false);
+console.log(false || true || false && false);
+console.log(false || false || false && false);
+console.log(true || false || false && false);
+
+console.log('.............................');
+
+console.log(true || true && true || true);
+console.log(true || true && true || false);
+console.log(true || true && false || true);
+console.log(true || false && true || true);
+console.log(false || true && true || true);
+console.log(true || true && false || false);
+console.log(true || false && true || falses);
+console.log(true || false && false || true);
+console.log(false || false && true || true);
+console.log(false || true && false || true);
+console.log(false || true && true || false);
+console.log(false || false && false || true);
+console.log(false || false && true || false);
+console.log(false || true && false || false);
+console.log(false || false && false || false);
+console.log(true || false && false || false);
+
+console.log('..............................');
+
+console.log(true && true || true || true);
+console.log(true && true || true || false);
+console.log(true && true || false || true);
+console.log(true && false || true || true);
+console.log(false && true || true || true);
+console.log(true && true || false || false);
+console.log(true && false || true || false);
+console.log(true && false || false || true);
+console.log(false && false || true || true);
+console.log(false && true || false || true);
+console.log(false && true || true || false);
+console.log(false && false || false || true);
+console.log(false && false || true || false);
+console.log(false && true || false || false);
+console.log(false && false || false || false);
+console.log(true && false || false || false);
+
+
+
